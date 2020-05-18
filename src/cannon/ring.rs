8080dda54d53fc2cd0dfb3a580e3f647c43ec9e9
asrcpq <asrcpq@outlook.com>
2020-05-18 17:15:50 +0000

    fix difficulty bug in ring

--- src/cannon/ring.rs
+++ src/cannon/ring.rs
@@ -36,7 +36,7 @@ impl CannonGeneratorInterface for Ring {
         let generated = simple_try(
             TRY_TIMES,
             |x| x[0] / x[1] * x[2],
-            vec![(200., 600.), (0.2, 3.5), (7., 90.)],
+            vec![(200., 600.), (2.0, 0.1), (7., 90.)],
             correlation,
             difficulty,
             rng.gen::<u64>(),
